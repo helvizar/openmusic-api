433dfd1562421e03d8465757c743e7f431c8c97f
Helvizar Muhammad <helvizaranderson@gmail.com>
2023-09-14 00:55:27 +0700

    feat: implement authentication functionality for testing openmusic v2

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 
+const ClientError = require('../../exceptions/ClientError');
+
 class AuthenticationsHandler {
   constructor(AuthenticationsService, UsersService, TokenManager, AuthenticationsValidator) {
     this._authenticationsService = AuthenticationsService;
@@ -9,26 +11,47 @@ class AuthenticationsHandler {
   }
 
   async postAuthenticationHandler(request, h) {
-    this._authenticationsValidator.validatePostAuthenticationPayload(request.payload);
+    try {
+      this._authenticationsValidator.validatePostAuthenticationPayload(request.payload);
 
-    const { username, password } = request.payload;
-    const id = await this._usersService.verifyUserCredential(username, password);
+      const { username, password } = request.payload;
+      const id = await this._usersService.verifyUserCredential(username, password);
 
-    const accessToken = this._tokenManager.generateAccessToken({ id });
-    const refreshToken = this._tokenManager.generateRefreshToken({ id });
+      const accessToken = this._tokenManager.generateAccessToken({ id });
+      const refreshToken = this._tokenManager.generateRefreshToken({ id });
 
-    await this._authenticationsService.addRefreshToken(refreshToken);
+      await this._authenticationsService.addRefreshToken(refreshToken);
 
-    const response = h.response({
-      status: 'success',
-      message: 'Authentication berhasil ditambahkan',
-      data: {
-        accessToken,
-        refreshToken,
-      },
-    });
-    response.code(201);
-    return response;
+      const response = h.response({
+        status: 'success',
+        message: 'Authentication berhasil ditambahkan',
+        data: {
+          accessToken,
+          refreshToken,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kesalahan pada server kami.',
+      });
+      response.code(500);
+      console.log(error);
+      return response;
+    }
   }
 
   async putAuthenticationHandler(request, h) {
